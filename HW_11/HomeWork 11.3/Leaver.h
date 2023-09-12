c0df9ff6717e4be871dc393e46b4983716c9e4b1
Vladilen <Vladbukugan1@gmail.com>
2023-09-12 11:51:03 +0300

    Correction HW11

--- HW_11/HomeWork 11.3/Leaver.h
+++ HW_11/HomeWork 11.3/Leaver.h
@@ -1,5 +1,5 @@
 #pragma once
-#ifdef LEAVER_EXPORTS
+#ifdef LeaverDinamicLib_EXPORTS
 #define LEAVER_API __declspec(dllexport)
 #else
 #define LEAVER_API __declspec(dllimport)
--- HW_11/HomeWork 11.5/Parallelogram.h
+++ HW_11/HomeWork 11.5/Parallelogram.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <string>
 #include "Quadrilateral.h"
 
 namespace Figure {
