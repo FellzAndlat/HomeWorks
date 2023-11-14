4a2ec28ac2df4d88de267291eb80b8c305f5eedb
FellzAndlat <Vladbukugan1@gmail.com>
2023-11-14 11:55:20 +0300

    add hw 1 and 2

---
+++ Advanced C++ Programming/HW_01/HomeWork_1_1/HomeWork_1_1/HomeWork_1_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::vector <int> arr{ 4, 7, 9, 14, 12 };
+
+    std::cout << "Входные данные: ";
+    std::for_each(
+        arr.begin(), arr.end(),
+        [](int i) {
+            std::cout << i << " ";
+        }
+    );
+    std::cout << "\n";
+
+    std::for_each(
+        arr.begin(), arr.end(),
+        [](int& i) {
+            if (i % 2) {
+                i = i * 3;
+            }
+        }
+    );
+
+    std::cout << "Выходные данные: ";
+    std::for_each(
+        arr.begin(), arr.end(),
+        [](int i) {
+            std::cout << i << " ";
+        }
+    );
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ Advanced C++ Programming/HW_01/HomeWork_1_2/HomeWork_1_2/HomeWork_1_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <variant>
+#include <vector>
+#include <string>
+#include <typeinfo>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+    std::srand(std::time(nullptr));
+    int random_variable = std::rand() % 3;
+
+    std::variant<int, std::string, std::vector<int>> result;
+    switch (random_variable)
+    {
+    case 0:
+        result = 5;
+        break;
+    case 1:
+        result = "string";
+        break;
+    case 2:
+        result = std::vector<int>{ 1, 2, 3, 4, 5 };
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+int main(){
+
+    if (std::holds_alternative <int>(get_variant())) {
+        auto result = std::get<int>(get_variant());
+        std::cout << result * 2;
+
+    }
+    else if (std::holds_alternative <std::string>(get_variant())) {
+        auto result = std::get<std::string>(get_variant());
+        std::cout << result;
+    }
+    else if (std::holds_alternative <std::vector <int>>(get_variant())) {
+        auto result = std::get<std::vector <int>>(get_variant());
+        for (int i = 0; i < result.size(); ++i) {
+            std::cout << result[i] << " ";
+        }
+    }
+}
