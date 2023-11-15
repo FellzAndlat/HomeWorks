72496ea9b5ccbe8695fdac49c252ff261bc1e6b7
FellzAndlat <Vladbukugan1@gmail.com>
2023-11-15 10:43:41 +0300

    add hw 3

---
+++ Advanced C++ Programming/HW_03/HomeWork_3_1/HomeWork_3_1/HomeWork_3_1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+class smart_array {
+private:
+    int* arr;
+    int size;
+    int i = 0;
+public:
+    void add_element(int num) {
+        if (i < size) {
+            arr[i] = num;
+            i += 1;
+        }
+        else {
+            throw std::exception("Выход за пределы заданного массива");
+        }
+    }
+    int get_element(int number) {
+        if (number < size) {
+            return arr[number];
+        }
+        else {
+            throw std::exception("Попытка обратиться к несуществующему элементу массива");
+        }
+    }
+    smart_array(int size) {
+        arr = new int[size];
+        this->size = size;
+    }
+    ~smart_array() {
+        delete arr;
+    }
+};
+
+
+int main()
+{
+    try{
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ Advanced C++ Programming/HW_03/HomeWork_3_2/HomeWork_3_2/HomeWork_3_2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+class smart_array {
+private:
+    int* arr;
+    int size;
+    int i = 0;
+public:
+    smart_array& operator=(const smart_array& src) {
+        size = src.size;
+        for (int j = 0; j < size; ++j) {
+            arr[j] = src.arr[j];
+        }
+
+        return *this;
+    }
+    void add_element(int num) {
+        if (i < size) {
+            arr[i] = num;
+            i += 1;
+        }
+        else {
+            throw std::exception("Выход за пределы заданного массива");
+        }
+    }
+    int get_element(int number) {
+        if (number < size) {
+            return arr[number];
+        }
+        else {
+            throw std::exception("Попытка обратиться к несуществующему элементу массива");
+        }
+    }
+    smart_array(int size) {
+        arr = new int[size];
+        this->size = size;
+    }
+    ~smart_array() {
+        delete arr;
+    }
+};
+
+int main() {
+    smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+    arr = new_array;
+
+    std::cout << arr.get_element(0) << arr.get_element(1);
+    
+}
