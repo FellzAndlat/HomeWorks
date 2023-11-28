0b8a689f8bdc36e9a1c8c9846373f662f17ae191
FellzAndlat <Vladbukugan1@gmail.com>
2023-11-28 13:55:25 +0300

    add hw 4++

--- Advanced C++ Programming/HW_04/HomeWork_4_2/Test_main.cpp
+++ Advanced C++ Programming/HW_04/HomeWork_4_2/Test_main.cpp
@@ -1,125 +1,57 @@
 #include <iostream>
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/catch_session.hpp"
-
-struct ListNode
-{
-public:
-    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
-        : value(value), prev(prev), next(next)
-    {
-        if (prev != nullptr) prev->next = this;
-        if (next != nullptr) next->prev = this;
-    }
-
-public:
-    int value;
-    ListNode* prev;
-    ListNode* next;
-};
-
-
-class List
-{
-public:
-    List()
-        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
-        m_tail(new ListNode(0, m_head))
-    {
-    }
-
-    virtual ~List()
-    {
-        Clear();
-        delete m_head;
-        delete m_tail;
-    }
-
-    bool Empty() { return m_size == 0; }
-
-    unsigned long Size() { return m_size; }
-
-    void PushFront(int value)
-    {
-        new ListNode(value, m_head, m_head->next);
-        ++m_size;
-    }
-
-    void PushBack(int value)
-    {
-        new ListNode(value, m_tail->prev, m_tail);
-        ++m_size;
-    }
-
-    int PopFront()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_head->next->next);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    int PopBack()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_tail);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    void Clear()
-    {
-        auto current = m_head->next;
-        while (current != m_tail)
-        {
-            current = current->next;
-            delete extractPrev(current);
-        }
-    }
-
-private:
-    ListNode* extractPrev(ListNode* node)
-    {
-        auto target = node->prev;
-        target->prev->next = target->next;
-        target->next->prev = target->prev;
-        --m_size;
-        return target;
-    }
-
-private:
-    ListNode* m_head;
-    ListNode* m_tail;
-    unsigned long m_size;
-};
-
-TEST_CASE("List PopBack testing", "[B_test]") {
-    List ListTest;
-    ListTest.PopBack();
-    CHECK(ListTest.Size() == 0);
-}
-
-TEST_CASE("List PopFront testing", "[F_test]") {
-    List ListTest;
-    ListTest.PopFront();
-    CHECK(ListTest.Size() == 0);
-}
+#include "List.cpp"
 
 TEST_CASE("List testing", "[A_test]") {
     List ListTest;
-    ListTest.PushBack(1);
-    CHECK(ListTest.Size() == 1);
-    ListTest.PopBack();
-    CHECK(ListTest.Size() == 0);
-    ListTest.PushFront(2);
-    ListTest.PushFront(2);
-    ListTest.PushFront(2);
-    CHECK(ListTest.Size() == 3);
-    ListTest.PopFront();
-    ListTest.PopFront();
-    CHECK(ListTest.Size() == 1);
+    
+    SECTION("Test popback and popfront on a blank sheet") {
+        CHECK_THROWS(ListTest.PopBack());
+        CHECK_THROWS(ListTest.PopFront());
+    }
+    SECTION("Test pushback and popback") {
+        ListTest.PushBack(1);
+        CHECK(ListTest.Size() == 1);
+        ListTest.PushBack(5);
+        ListTest.PushBack(3);
+        ListTest.PushBack(10);
+        CHECK(ListTest.Size() == 4);
+        ListTest.PushBack(456);
+        ListTest.PushBack(223);
+        CHECK(ListTest.Size() == 6);
+
+        ListTest.PopBack();
+        CHECK(ListTest.Size() == 5);
+        ListTest.PopBack();
+        ListTest.PopBack();
+        CHECK(ListTest.Size() == 3);
+        ListTest.PopBack();
+        ListTest.PopBack();
+        ListTest.PopBack();
+        CHECK(ListTest.Size() == 0);
+    }
+    SECTION("Test pushfront and popfront") {
+        ListTest.PushFront(2);
+        ListTest.PushFront(22);
+        ListTest.PushFront(21);
+        CHECK(ListTest.Size() == 3);
+        ListTest.PushFront(25);
+        ListTest.PushFront(28);
+        CHECK(ListTest.Size() == 5);
+        ListTest.PushFront(23);
+        CHECK(ListTest.Size() == 6);
+
+        ListTest.PopFront();
+        ListTest.PopFront();
+        CHECK(ListTest.Size() == 4);
+        ListTest.PopFront();
+        CHECK(ListTest.Size() == 3);
+        ListTest.PopFront();
+        ListTest.PopFront();
+        ListTest.PopFront();
+        CHECK(ListTest.Size() == 0);
+    }
 
 
 }
