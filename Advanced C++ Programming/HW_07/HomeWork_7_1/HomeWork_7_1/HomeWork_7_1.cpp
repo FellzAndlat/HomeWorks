b324c6a1cd4abec1b292074d847da295dc8e998a
FellzAndlat <Vladbukugan1@gmail.com>
2023-12-13 16:36:27 +0300

    add hw 7

---
+++ Advanced C++ Programming/HW_07/HomeWork_7_1/HomeWork_7_1/HomeWork_7_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <class type>
+void sort_arr(type& arr, int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::vector <int> arr = { 1,1,2,5,6,1,2,4 };
+
+    std::cout << "[in]: ";
+    for (int i = 0; i < arr.size(); ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    sort_arr(arr, arr.size());
+    
+    auto it = std::unique(arr.begin(), arr.end());
+    arr.erase(it, arr.end());
+
+    std::cout << "[out]: ";
+    for (int i = 0; i < arr.size(); ++i) {
+        std::cout << arr[i] << " ";
+    }
+}
---
+++ Advanced C++ Programming/HW_07/HomeWork_7_2/HomeWork_7_2/HomeWork_7_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+template <class type>
+void print_container(type& arr) {
+    for (auto i : arr) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
