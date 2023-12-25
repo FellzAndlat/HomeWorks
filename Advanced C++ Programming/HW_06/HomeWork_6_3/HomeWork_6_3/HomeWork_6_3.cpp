529cd935f1640d120ae05a9d81d01bf9ca14439d
FellzAndlat <Vladbukugan1@gmail.com>
2023-12-26 02:37:01 +0300

    add hw 6++

--- Advanced C++ Programming/HW_06/HomeWork_6_3/HomeWork_6_3/HomeWork_6_3.cpp
+++ Advanced C++ Programming/HW_06/HomeWork_6_3/HomeWork_6_3/HomeWork_6_3.cpp
@@ -9,12 +9,14 @@ private:
     type* arr = new type [capacit];
 public:
     vector& operator = (const vector& src) {
-        delete[] arr;
-        siz = src.siz;
-        capacit = src.capacit;
-        arr = new type[capacit];
-        for (int i = 0; i < capacit; ++i) {
-            arr[i] = src.arr[i];
+        if (this != &src) {
+            delete[] arr;
+            siz = src.siz;
+            capacit = src.capacit;
+            arr = new type[capacit];
+            for (int i = 0; i < capacit; ++i) {
+                arr[i] = src.arr[i];
+            }
         }
         return *this;
     }
@@ -44,14 +46,23 @@ public:
     type at(int index) {
         try {
             if (index >= siz || index < 0) {
-                throw std::exception("Обращение за пределы массива");
+                throw std::runtime_error("Обращение за пределы массива");
             }
             else {
                 return arr[index];
             }
         }
-        catch (const std::exception& ex) {
+        catch (const std::runtime_error& ex) {
             std::cout << ex.what() << std::endl;
+        }
+    }
+    vector() {}
+    vector(const vector& src) {
+        siz = src.siz;
+        capacit = src.capacit;
+        arr = new type[capacit];
+        for (int i = 0; i < capacit; ++i) {
+            arr[i] = src.arr[i];
         }
     }
     ~vector() {
@@ -71,15 +82,19 @@ int main()
     arr.push_back(5);
     arr.push_back(3);
 
-    std::cout << arr.at(3) << " " << arr.size() << " " << arr.capacity() << std::endl;
+    std::cout << arr.at(-1) << " " << arr.size() << " " << arr.capacity() << std::endl;
     arr.push_back(2);
     std::cout << arr.size() << " " << arr.capacity() << std::endl;
 
     vector <int> arr2;
 
+    vector <int> arr3(arr);
+
     arr = arr2;
 
-    std::cout << arr.size();
+    std::cout << arr.size() << std::endl;
+
+    std::cout << arr3.at(1) << " " << arr3.size() << " " << arr3.capacity() << std::endl;
 
     return 0;
 }
---
+++ Advanced C++ Programming/HW_09/HomeWork_9_1/HomeWork_9_1/HomeWork_9_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class type>
+void move_vectors(std::vector <type>& one, std::vector <type>& two) {
+    std::vector <type> temp = std::move(one);
+    one = std::move(two);
+    two = std::move(temp);
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one, two);
+    for (int i = 0; i < two.size(); ++i) {
+        std::cout << two[i] << " ";
+    }
+}
