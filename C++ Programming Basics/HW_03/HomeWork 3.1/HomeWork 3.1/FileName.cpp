1f947c9b5f5c9842aa70d00096c3ff2cff15a56c
Миллер Влад <Vladbukugan1@gmail.com>
2024-10-08 11:46:07 +0300

    HW8 updata

---
+++ C++ Programming Basics/HW_03/HomeWork 3.1/HomeWork 3.1/FileName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class testdlapidora {
+private:
+	int	pidor1 = 10;
+public:
+	void print() {
+		std::cout << "ti pidor";
+	}
+};
+
+
+int main() {
+	testdlapidora rar;
+	rar.print();
+}
