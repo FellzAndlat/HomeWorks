e0e52e595a48100f6f7ca48c472e5f0425cd0da0
Миллер Влад <Vladbukugan1@gmail.com>
2024-11-20 15:31:38 +0300

    AirportInspector updata

--- QT/AirportInspector/build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AirportInspector_FILE C:/Users/mrsta/HomeWorks/QT/AirportInspector/build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/AirportInspector.exe)
-set(__QT_DEPLOY_TARGET_AirportInspector_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AirportInspector_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
